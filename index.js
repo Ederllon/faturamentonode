88731c40f8e0d9c7ff83f0f52620904adf57214c
Ederllon <156702239+Ederllon@users.noreply.github.com>
2025-03-13 17:47:07 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const faturamento = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+};
+
+// Calcula o faturamento total
+const faturamentoTotal = Object.values(faturamento).reduce((acc, valor) => acc + valor, 0);
+
+// Calcula e exibe os percentuais
+console.log("Percentual de Representação por Estado:");
+for (const estado in faturamento) {
+    const percentual = (faturamento[estado] / faturamentoTotal) * 100;
+    console.log(`${estado}: ${percentual.toFixed(2)}%`);
+}
